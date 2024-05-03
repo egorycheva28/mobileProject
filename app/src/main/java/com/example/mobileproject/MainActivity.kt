7275008df0c7a9667885ac9687c5c8f99e932ab6
Zayata Budaeva <budaevazay@mail.ru>
2024-05-03 12:36:30 +0700

    дизайн: кнопка камеры и галереи, фон

--- app/src/main/java/com/example/mobileproject/MainActivity.kt
+++ app/src/main/java/com/example/mobileproject/MainActivity.kt
@@ -3,100 +3,53 @@ package com.example.mobileproject
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
-import android.widget.Button
-import android.widget.ImageView
-import androidx.appcompat.app.AppCompatActivity
-import android.graphics.Bitmap
-import android.graphics.Matrix
 import android.provider.MediaStore
+import androidx.appcompat.app.AppCompatActivity
+import android.widget.ImageButton
+
 class MainActivity : AppCompatActivity() {
-    val REQUEST_GALLERY=100
-    private var nBitmap:Bitmap?=null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        //enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
-        var myButton = findViewById(R.id.fragment1) as Button
-        myButton.setOnClickListener {
-            intent = Intent(Intent.ACTION_PICK)
-            intent.type = "image/*"
-            startActivityForResult(intent, REQUEST_GALLERY)
-        }
-        var myButton1 = findViewById(R.id.button2) as Button
-        myButton1.setOnClickListener{
-            var bitmap=nBitmap
-            var newBitmap=rotateImage(bitmap);
-            nBitmap=newBitmap;
-            var imageView = findViewById(R.id.imageView1) as ImageView
-            imageView.setImageBitmap(newBitmap)
+        val cameraButton = findViewById<ImageButton>(R.id.camera_button)
+        val galleryButton = findViewById<ImageButton>(R.id.gallery_button)
 
+        // нажатие на кнопку камеры
+        cameraButton.setOnClickListener {
+            takePicture()
+        }
+
+        // нажатие на кнопку галереи
+        galleryButton.setOnClickListener {
+            importFromGallery()
         }
     }
 
-    private fun rotateImage(bitmap: Bitmap?): Bitmap {
-        var matrix = Matrix()
-        matrix.postRotate(90.0f)
-
-        return Bitmap.createBitmap(bitmap!!, 0, 0, bitmap.width, bitmap.height, matrix, true)
-    }
-
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-
-        if (resultCode == RESULT_OK && requestCode == REQUEST_GALLERY) {
-
-            var imageUri = data?.data
-            nBitmap = MediaStore.Images.Media.getBitmap(contentResolver, imageUri)
-
-            show_URI_as_Bitmap(imageUri)
-
+    private fun takePicture() {
+        val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        try {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
+        } catch (e: Exception) {
+            // Обработка ошибки, если приложение камеры недоступно
+            e.printStackTrace()
         }
     }
-    fun show_URI_as_Bitmap(uri: Uri?)
-    {
-        var myImageView = findViewById(R.id.imageView1) as ImageView
-        //val inputStream = contentResolver.openInputStream(uri)
-        //val bitmap = BitmapFactory.decodeStream(inputStream)
-        myImageView.setImageURI(uri)
+
+    private fun importFromGallery() {
+        val openGalleryIntent =
+            Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+        try {
+            startActivityForResult(openGalleryIntent, REQUEST_IMAGE_OPEN)
+        } catch (e: Exception) {
+            // Обработка ошибки, если приложение галереи недоступно
+            e.printStackTrace()
+        }
     }
 
-    /*fun rotateImage(bitmap: Bitmap): Bitmap {
-        val matrix = Matrix()
-        matrix.postRotate(90.0f)
-
-        return Bitmap.createBitmap(bitmap, 0, 0, bitmap.width, bitmap.height, matrix, true)
-    }*/
-    //val newBitmap=rotateImage(bitmap);
-    //val imageView = findViewById(R.id.imageView1) as ImageView
-    //imageView.setImageBitmap(newBitmap)
+    companion object {
+        private const val REQUEST_IMAGE_CAPTURE = 1
+        private const val REQUEST_IMAGE_OPEN = 2
+    }
 }
-        /*ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }*/
-        /*val myButton = findViewById(R.id.fragment1) as Button
-        val frameLayout = findViewById(R.id.frame_layout) as FrameLayout
-        fun setNewFragment()
-        {
-            val fragment=FirstFragment()
-            getSupportFragmentManager().beginTransaction()
-                .add(R.id.frame_layout,fragment)
-                .replace(R.id.frame_layout,fragment)
-
-        }
-        myButton.setOnClickListener {
-            val fragment = FirstFragment()
-            supportFragmentManager.beginTransaction()
-                //.add(R.id.frame_layout,fragment)
-                .replace(R.id.frame_layout, fragment)
-                .addToBackStack(null)
-                .commit()
-            val intent=Intent(Intent.ACTION_PICK)
-            intent.type="image/*"
-            startActivityForResult(intent,REQUEST_GALLERY)
-        }*/
-
-         */
-
