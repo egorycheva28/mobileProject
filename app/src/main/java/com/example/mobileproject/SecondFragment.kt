ab4454465d18027a337a72d632e6efdf6d88d800
egorycheva28 <egor.masha28@list.ru>
2024-05-15 12:03:29 +0700

    работа с фрагментами

--- app/src/main/java/com/example/mobileproject/SecondFragment.kt
+++ app/src/main/java/com/example/mobileproject/SecondFragment.kt
@@ -5,9 +5,17 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
+import android.widget.SeekBar
+
 class SecondFragment : Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val activity = requireActivity() as MainActivity3
+        val button1 = requireView().findViewById(R.id.filter_button1) as ImageButton
+        button1.setOnClickListener {
+            activity.ggg();
+        }
     }
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
--- app/src/main/java/com/example/mobileproject/ThirdFragment.kt
+++ app/src/main/java/com/example/mobileproject/ThirdFragment.kt
@@ -1,23 +1,20 @@
 package com.example.mobileproject
 
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.SeekBar
-import androidx.fragment.app.Fragment
-class FirstFragment : Fragment() {
+
+class ThirdFragment : Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val seekBar = requireView().findViewById(R.id.seekBar) as SeekBar
-        val num=seekBar.getProgress()
-
     }
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-
-        return inflater.inflate(R.layout.fragment_first2, container, false)
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_third, container, false)
     }
 }
